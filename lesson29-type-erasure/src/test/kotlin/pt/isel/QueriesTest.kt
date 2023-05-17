80e80055eef4ef6bdd7c697571353a5ac4d26ca7
Miguel Gamboa <miguelgamboa@outlook.com>
2023-05-17 16:04:03 +0100

    Setup lesson 29

---
+++ lesson29-type-erasure/src/test/kotlin/pt/isel/QueriesTest.kt
@@ -0,0 +1,19 @@
+package pt.isel
+
+import org.junit.jupiter.api.Test
+import java.io.BufferedWriter
+import java.io.Closeable
+import java.io.File
+import java.io.FileOutputStream
+import java.lang.ref.Cleaner
+import java.nio.channels.OverlappingFileLockException
+import java.util.Random
+import kotlin.test.assertEquals
+import kotlin.test.assertFailsWith
+
+class QueriesTest {
+    @Test fun fooTest() {
+
+    }
+
+}
